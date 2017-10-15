d2568b5a57e08664c4547c9d591e85ece0e5f672
Brian Murphy-Dye <brian@murphydye.com>
2017-10-15 17:24:10 -0600

    Add some more eww stuff. SPC ob to browse, SPC of to browse in firefox.

--- spacemacs/my-eww.el
+++ spacemacs/my-eww.el
@@ -83,3 +83,60 @@
        (define-key eww-mode-map "o" 'ace-link-eww))))
 
 (ace-link-setup-default)
+
+
+
+;; https://github.com/dakrone/eos/blob/master/eos-web.org
+(use-package eww
+  :defer t
+  :init
+  (setq browse-url-browser-function
+        '((".*google.*maps.*" . browse-url-generic)
+          ;; Github goes to firefox, but not gist
+          ("http.*\/\/github.com" . browse-url-generic)
+          ("groups.google.com" . browse-url-generic)
+          ("docs.google.com" . browse-url-generic)
+          ("melpa.org" . browse-url-generic)
+          ("build.*\.elastic.co" . browse-url-generic)
+          (".*-ci\.elastic.co" . browse-url-generic)
+          ("internal-ci\.elastic\.co" . browse-url-generic)
+          ("zendesk\.com" . browse-url-generic)
+          ("salesforce\.com" . browse-url-generic)
+          ("stackoverflow\.com" . browse-url-generic)
+          ("apache\.org\/jira" . browse-url-generic)
+          ("thepoachedegg\.net" . browse-url-generic)
+          ("zoom.us" . browse-url-generic)
+          ("t.co" . browse-url-generic)
+          ("twitter.com" . browse-url-generic)
+          ("\/\/a.co" . browse-url-generic)
+          ("youtube.com" . browse-url-generic)
+          ("amazon.com" . browse-url-generic)
+          ("." . eww-browse-url)))
+  (setq shr-external-browser 'browse-url-generic)
+  (setq browse-url-generic-program (executable-find "firefox"))
+  (add-hook 'eww-mode-hook #'toggle-word-wrap)
+  (add-hook 'eww-mode-hook #'visual-line-mode)
+  :config
+  (use-package s :ensure t)
+  (define-key eww-mode-map "o" 'eww)
+  (define-key eww-mode-map "O" 'eww-browse-with-external-browser)
+  (define-key eww-mode-map "j" 'next-line)
+  (define-key eww-mode-map "k" 'previous-line)
+
+  (use-package eww-lnum
+    :ensure t
+    :config
+    (bind-key "f" #'eww-lnum-follow eww-mode-map)
+    (bind-key "U" #'eww-lnum-universal eww-mode-map)))
+
+(use-package link-hint
+  :ensure t
+  :bind ("C-c f" . link-hint-open-link))
+
+(defun browse-last-url-in-brower ()
+  (interactive)
+  (save-excursion
+    (ffap-next-url t t)))
+
+(global-set-key (kbd "of") 'browse-last-url-in-brower) ;; firefox
+(global-set-key (kbd "ob") 'browse-url-at-point)
