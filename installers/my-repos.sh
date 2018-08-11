7c7e8c8e58560e72469dc1f358982e1e011f09ce
Brian Murphy-Dye <brian@murphydye.com>
2018-08-11 10:11:56 -0600

    Add installer for my repos

---
+++ installers/my-repos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p ~/code/me
+cd ~/code/me
+
+# ruby
+git clone git@github.com:brianmd/trellor.git
+git clone git@github.com:brianmd/collectr.git
+
+
+# clojure
+git clone git@gitlab.com:sludge/asnode.git
+git@github.com:brianmd/timmus.git
+git@github.com:brianmd/rosetta.git
+git@github.com:brianmd/rampart.git
+git clone git@github.com:brianmd/mishmash.git
+gti clone git@github.com:brianmd/treadstone.git
+
+git clone git@gitlab.com:sludge/haystack-ui.git
