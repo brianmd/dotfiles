5ef4aafa39035bb3c32f2bd12f2611b12ec14134
Brian Murphy-Dye <brian.murphy-dye@thetradedesk.com>
2019-05-19 08:20:59 -0600

    wip

--- spacemacs/myspace.el
+++ spacemacs/myspace.el
@@ -148,7 +148,58 @@ directory to make multiple eshell windows easier."
 
 (require 'cl-lib)
 
-debug-msg "fonts ...")
+;; from http://aaronbedra.com/emacs.d/
+;; (require 'f)
+
+(setq eshell-visual-commands
+      '("less" "tmux" "htop" "top" "bash" "zsh" "fish"))
+
+(setq eshell-visual-subcommands
+      '(("git" "log" "l" "diff" "show")))
+
+;; ;; Prompt with a bit of help from http://www.emacswiki.org/emacs/EshellPrompt
+;; (defmacro with-face (str &rest properties)
+;;   `(propertize ,str 'face (list ,@properties)))
+
+;; (defun eshell/abbr-pwd ()
+;;   (let ((home (getenv "HOME"))
+;;         (path (eshell/pwd)))
+;;     (cond
+;;      ((string-equal home path) "~")
+;;      ((f-ancestor-of? home path) (concat "~/" (f-relative path home)))
+;;      (path))))
+
+;; (defun eshell/my-prompt ()
+;;   (let ((header-bg "#161616"))
+;;     (concat
+;;      (with-face (eshell/abbr-pwd) :foreground "#008700")
+;;      (if (= (user-uid) 0)
+;;          (with-face "#" :foreground "red")
+;;        (with-face "$" :foreground "#2345ba"))
+;;      " ")))
+
+;; (setq eshell-prompt-function 'eshell/my-prompt)
+;; (setq eshell-highlight-prompt nil)
+;; (setq eshell-prompt-regexp "^[^#$\n]+[#$] ")
+
+;; (setq eshell-cmpl-cycle-completions nil)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+
+(message "Deleting old backup files...")
+(let ((week (* 60 60 24 7))
+      (current (float-time (current-time))))
+  (dolist (file (directory-files temporary-file-directory t))
+    (when (and (backup-file-name-p file)
+               (> (- current (float-time (fifth (file-attributes file))))
+                  week))
+      (message "%s" file)
+      (delete-file file))))
+
+(debug-msg "fonts ...")
 ;;; Monaco font for programming (and some other modes)
 ;; from https://www.reddit.com/r/emacs/comments/73lplp/what_are_your_preferred_fonts_in_emacs/
 (defvar dh-monaco-face-remapping-alist nil)
